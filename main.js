9ce3c91e8caf82e046a4a1b185aadb4098934dc6
Linus Hammarberg1 <Linus.hammarberg@gmail.com>
2022-12-06 12:55:05 +0100

    Version 1.0 complete. Will keep work, handing in.

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-const bild1 = "/personal-website/images/sand-scrolling-background"
-const bild2 = "/personal-website/images/Background-1"
-const bild3 = "";
-var backGroundEffects = [
+const bild1 = "./backgrounds/sand-scrolling-background.avif";
+const bild2 = "./backgrounds/pexels-photo-924824.jpeg";
+const bild3 = "/backgrounds/Starry sand.jpeg";
+const bild4 = "/backgrounds/triangle.webp";
+const bild5 = "/backgrounds/Wood.jpeg";
+
+let currentIndex = 0;
+var backgroundEffects = [
 
     bild1,
 
     bild2,
 
     bild3,
+
+    bild4,
+
+    bild5
 ]
 ;
 
@@ -15,47 +23,16 @@ document.addEventListener("keydown" , (event) => {
     changeBackground(event.key);
 });
 
-function changeBackgroundBack(){
-    for (let i = 0; i < backGroundEffects.length; i++)
-        document.getElementsByTagName("BODY").style.background = backgroundEffects[i] + 1;
-}
-
-function changeBackgroundForward(){
-for (let i = backGroundEffects.length; i > 1; i--)
-    document.getElementsByTagName("BODY").style.background = backgroundEffects[i] - 1;
-}
-
-var currentBackground = backGroundEffects[1]
-
- function changeBackgroundBack(){
-    document.getElementsByTagName("BODY").style.background = backgroundEffects[i] - 1;
-     let currentBackground = backgroundEffects[i] - 1
- }
-
- function changeBackgroundForward() {
-     document.getElementsByTagName("BODY").style.background = backgroundEffects[i] + 1;
-     let currentBackground = backgroundEffects[i] + 1
- };
-
- function changeBackground(){
-     if (event.key == "ArrowLeft"){
-         document.getElementsByTagName("BODY").style.background = backgroundEffects[i] - 1;
-     }
+ function changeBackground(key){
+     if (key == "ArrowLeft"){
+        currentIndex--
+    }
     else if (event.key == "ArrowRight"){
-         document.getElementsByTagName("BODY").style.background = backgroundEffects[i] + 1;
-     }
+        currentIndex++;
+    }
+    const nextImage = backgroundEffects[currentIndex]
+    document.body.style.background = `url(${nextImage})`;
  }
-
-function arrowPress(){
-     switch (event.key) {
-         case "ArrowLeft":
-             changeBackgroundBack()
-             break;
-         case "ArrowRight":
-             changeBackgroundForward()
-            break;
-     }
-};
 
 
 
